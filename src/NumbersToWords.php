ddf7c2eab901673c23948a49563a16073a2b31e5
jared beckler and matt knutson <mknut001@gmail.com>
2016-02-18 14:06:04 -0800

    add first working spec

--- src/NumbersToWords.php
+++ src/NumbersToWords.php
@@ -1,9 +1,21 @@
 <?php
     class NumbersToWords
     {
-        function convertToWords()
+        function convertToWords($input_number)
         {
-            
+            $onesPlace = array(
+                "1" => "One",
+                "2" => "Two",
+                "3" => "Three",
+                "4" => "Four",
+                "5" => "Five",
+                "6" => "Six",
+                "7" => "Seven",
+                "8" => "Eight",
+                "9" => "Nine"
+                );
+
+            return $onesPlace[$input_number];
         }
     }
 
--- tests/NumbersToWordsTest.php
+++ tests/NumbersToWordsTest.php
@@ -3,6 +3,14 @@
 
     class NumbersToWordsTest extends PHPUnit_Framework_TestCase
     {
-        
+        function test_numbersToWords_oneDigit()
+        {
+            $test_numbersToWords_oneDigit = new NumbersToWords;
+            $input = "1";
+
+            $result = $test_numbersToWords_oneDigit->convertToWords($input);
+
+            $this->assertEquals("One", $result);
+        }
     }
 ?>
